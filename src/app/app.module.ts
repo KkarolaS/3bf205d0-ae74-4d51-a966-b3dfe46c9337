bf737f9f0476bca5be32c94ed158b796dc6af8be
KkarolaS <133031923+KkarolaS@users.noreply.github.com>
2024-02-01 07:07:22 +0100

    create basic components and add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { EventsListComponent } from './home/events-list/events-list.component';
+import { EventItemComponent } from './home/events-list/event-item/event-item.component';
+import { BasketComponent } from './basket/basket.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, EventsListComponent, EventItemComponent, BasketComponent],
+  imports: [BrowserModule, TooltipModule.forRoot()],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
